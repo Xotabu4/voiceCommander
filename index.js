f0753e39b7b944b718b399f26b01a6b2d04c3969
Oleksandr Khotemskyi <xotabu4@gmail.com>
2018-08-19 22:18:11 +0300

    Added selenoid status

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const express = require('express')
+const request = require('request-promise-native')
 const app = express()
 
-app.post('/', (req, res) => {
+app.post('/', async (req, res) => {
     console.log('===INCOMMING REQUEST===')
     console.log('===HEADERS===')
     console.log(req.headers)
     console.log('===BODY===')
     console.log(req.body)
-    res.send({ fulfillmentText: 'This is text response from server!' })
+    let body = await request.get('http://ip-5236.sunline.net.ua:4444/status', { json: true })
+    console.log(body)
+    let text = `Used ${body.used} out of ${body.total} browser sessions. `
+    if (body.queued > 0) {
+        text = text + `And also there are queue of ${body.queued} sessions`
+    }
+    if (body.queued > 0) {
+        text = text + `And also there are ${body.pending} of pending sessions`
+    }
+    res.send({ fulfillmentText: text })
 })
 
 app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port: ' + process.env.PORT))
